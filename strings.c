2caae6fcdeaa7705439bcb93c505288e8cf9320a
victornnamdii <101352050+victornnamdii@users.noreply.github.com>
2022-06-15 22:14:32 +0100

    Update strings.c

--- strings.c
+++ strings.c
@@ -1,97 +1,93 @@
 #include "main.h"
-/**
- * _strcmp - compares two strings
- * @s1: char typr pointer
- * @s2: char type pointer
- * Return: difference in the values
- */
-int _strcmp(char *s1, char *s2)
-{
-	int i, m;
 
-	for (i = 0; s1[i] != '\0'; i++)
-	{
-		m = s1[i] - s2[i];
-		if (m != 0)
-			break;
-	}
-	if (m == 0)
-		m = s1[i] - s2[i];
-	return (m);
-}
 /**
- * _strcpy - copies src to dest
- * @dest: char type pointer
- * @src: char type pointer
- * Return: dest
+ * _strlen - returns the length of a string
+ * @s: string s
+ * Return: length of string
  */
-char *_strcpy(char *dest, char *src)
+int _strlen(char *s)
 {
-	int i;
+	char *p = s;
 
-	for (i = 0; src[i] != '\0'; i++)
-	{
-		dest[i] = src[i];
-	}
-	dest[i] = '\0';
-	return (dest);
+	while (*s)
+		s++;
+	return (s - p);
 }
-/**
- * _puts - prints a string followed by a new line to stdout
- * @str: char type pointer
- */
-void _puts(char *str)
-{
-	int i = 0;
 
-	for (i = 0; str[i] != '\0'; i++)
-	{
-		_putchar(str[i]);
-	}
-	_putchar(10);
-}
 /**
  * _strcat - concatenates two strings
- * @dest: char type pointer
- * @src: char type pointer
- * Return: a pointer to the resulting string dest
+ * @dest: copy destination
+ * @src: copy source
+ * Return: concatenated string
  */
 char *_strcat(char *dest, char *src)
 {
-	int i, j, k;
+	char *ptr = dest + _strlen(dest);
 
-	for (i = 0; dest[i] != '\0'; i++)
-		;
-	k = i;
-	for (i = k, j = 0; src[j] != '\0'; i++, j++)
-	{
-		dest[i] = src[j];
-		dest[i + 1] = '\0';
-	}
+	while (*src)
+		*ptr++ = *src++;
+	*ptr = 0;
 	return (dest);
 }
-/**
- * _atoi - converts a string to an integer
- * @s: char  type pointer
- * Return: 0 if no number else the converted value
- */
-int _atoi(char *s)
-{
-	int i, sign = 1;
-	unsigned int n = 0;
 
-	for (i = 0; s[i] != '\0'; i++)
+/**
+ * _strcmp - compares two strings
+ * @s1: string 1
+ * @s2: string 2
+ * Return: 1 if true, 0 if false
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1)
 	{
-		if (s[i] == 45)
-		{
-			sign *= -1;
-		}
-		else if (s[i] >= 48 && s[i] <= 57)
-		{
-			n = (n * 10) + (s[i] - '0');
-		}
-		else if (n > 0)
+		if (*s1 != *s2)
 			break;
+		s1++;
+		s2++;
 	}
-	return (sign * n);
+
+	return (*(unsigned char *)s1 - *(unsigned char *)s2);
+}
+
+/**
+ * _strdup - returns a pointer to a newly allocated space in memory,
+ * which contains a copy of the string given as a parameter.
+ * @str: string to be copied
+ * Return: copied string
+ */
+char *_strdup(char *str)
+{
+	int i, len;
+	char *copy;
+
+	if (!str)
+		return (NULL);
+	len = _strlen(str);
+	copy = malloc(sizeof(char) * len + 1);
+	if (!copy)
+	{
+		perror("Malloc failed\n");
+		exit(errno);
+	}
+	for (i = 0; i < len; i++)
+		copy[i] = str[i];
+	copy[i] = 0;
+	return (copy);
+}
+
+/**
+ * *_strcpy - copies the string pointed to by src,
+ * including the terminating null byte
+ * @dest: copied string
+ * @src: string to be copied
+ * Return: pointer to new copied string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	char *ptr = dest;
+
+	while (*src)
+		*dest++ = *src++;
+	*dest = 0;
+	return (ptr);
 }
