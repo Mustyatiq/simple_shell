0ca41ebd18d7900605901360a7c613793940ffce
victornnamdii <101352050+victornnamdii@users.noreply.github.com>
2022-06-15 22:16:02 +0100

    Update strings3.c

--- strings3.c
+++ strings3.c
@@ -1,62 +1,40 @@
 #include "main.h"
-/**
- * get_len - gets number length
- * @n: integer
- * Return: length
- */
-int get_len(int n)
-{
-	unsigned int n1;
-	int len = 1;
 
-	if (n < 0)
-	{
-		len++;
-		n1 = n * -1;
-	}
-	else
-		n1 = n;
-	while (n1 > 9)
-	{
-		len++;
-		n1 = n1 / 10;
-	}
-	return (len);
+/**
+ * insertNullByte - inserts null byte at given index
+ * @str: input string
+ * @index: index to insert null byte
+ */
+void insertNullByte(char *str, unsigned int index)
+{
+	str[index] = '\0';
 }
+
 /**
- * _itoa - function converts int to string.
- * @n: type int number
- * Return: String.
+ * displayPrompt - displays shell prompt
  */
-char *_itoa(int n)
+void displayPrompt(void)
 {
-	unsigned int n1;
-	int lenght = get_len(n);
-	char *buffer;
+	write(STDERR_FILENO, "$ ", 2);
+}
 
-	buffer = malloc(sizeof(char) * (lenght + 1));
-	if (buffer == 0)
-		return (NULL);
+/**
+ * displayNewLine - displays new line
+ */
+void displayNewLine(void)
+{
+	write(STDOUT_FILENO, "\n", 1);
+}
 
-	*(buffer + lenght) = '\0';
-
-	if (n < 0)
-	{
-		n1 = n * -1;
-		buffer[0] = '-';
-	}
-	else
-	{
-		n1 = n;
-	}
-
-	lenght--;
-	do {
-		*(buffer + lenght) = (n1 % 10) + '0';
-		n1 = n1 / 10;
-		lenght--;
-	}
-	while (n1 > 0)
-		;
-	return (buffer);
+/**
+ * sigintHandler - catches SIGINT signal and reset signal
+ * @sigint: signal from stdint
+ */
+void sigintHandler(int sigint)
+{
+	(void)sigint;
+	signal(SIGINT, sigintHandler);
+	displayNewLine();
+	displayPrompt();
+	fflush(stdout);
 }
