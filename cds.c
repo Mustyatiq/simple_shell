a1e7f588f1aaddce2a609fd7823bc20ae3da1034
victornnamdii <101352050+victornnamdii@users.noreply.github.com>
2022-06-15 21:58:39 +0100

    Update cds.c

--- cds.c
+++ cds.c
@@ -1,144 +1,59 @@
 #include "main.h"
-/**
- * cd_dot - changes to parent directory
- * @command: comand passed
- * @data: struct data
- */
-void cd_dot(char **command, store *data)
-{
-	char pwd[PATH_MAX];
-	char *hold, *cp_pwd, *cp_strtok_pwd;
 
-	getcwd(pwd, sizeof(pwd));
-	cp_pwd = _strdup(pwd);
-	set_env("OLDPWD", cp_pwd, data);
-	hold = command[1];
-	if (_strcmp(".", hold) == 0)
+/**
+ * updateOld - updates OLDPWD to current PWD
+ * @build: input build
+ * Return: index in linked list of PWD on success -
+ * If PWD or OLDPWD does not exist in environ vars,
+ * return -1
+ */
+int updateOld(config *build)
+{
+	register int pwdIndex = 0, index = 0;
+	static char old[BUFSIZE];
+	char *current = NULL;
+
+	_strcat(old, "OLD");
+	pwdIndex = searchNode(build->env, "PWD");
+	if (pwdIndex == -1)
 	{
-		set_env("PWD", cp_pwd, data);
-		free(cp_pwd);
-		return;
+		return (-1);
 	}
-	if (_strcmp("/", cp_pwd) == 0)
+	current = getNodeAtIndex(build->env, pwdIndex);
+	_strcat(old, current);
+	insertNullByte(old, _strlen(current) + 4);
+	free(current);
+	index = searchNode(build->env, "OLDPWD");
+	if (index == -1)
 	{
-		free(cp_pwd);
-		return;
+		return (-1);
 	}
-	cp_strtok_pwd = cp_pwd;
-	rev_string(cp_strtok_pwd);
-	cp_strtok_pwd = _strtok(cp_strtok_pwd, "/");
-	if (cp_strtok_pwd != NULL)
-	{
-		cp_strtok_pwd = _strtok(NULL, "\0");
-		if (cp_strtok_pwd != NULL)
-			rev_string(cp_strtok_pwd);
-	}
-	if (cp_strtok_pwd != NULL)
-	{
-		chdir(cp_strtok_pwd);
-		set_env("PWD", cp_strtok_pwd, data);
-	}
-	else
-	{
-		chdir("/");
-		set_env("PWD", "/", data);
-	}
-	data->_return = 0;
-	free(cp_pwd);
+	deleteNodeAtIndex(&build->env, index);
+	addNodeAtIndex(&build->env, index, old);
+	insertNullByte(old, 0);
+	return (pwdIndex);
 }
+
 /**
- * cd_to - changes to a given directory
- * @command: command passed
- * @data: struct store
+ * updateCur - updates PWD to accurately reflect current directory
+ * @build: input build
+ * @index: index in linked list of where to insert PWD env var
+ * Return: true on success, false on failure
  */
-void cd_to(char **command, store *data)
+_Bool updateCur(config *build, int index)
 {
-	char pwd[PATH_MAX];
-	char *hold, *cp_pwd, *cp_dir;
+	static char tmp[BUFSIZE], cwd[BUFSIZE];
 
-	getcwd(pwd, sizeof(pwd));
-
-	hold = command[1];
-	if (chdir(hold) == -1)
+	getcwd(tmp, BUFSIZE);
+	_strcat(cwd, "PWD=");
+	_strcat(cwd, tmp);
+	if (index > -1)
 	{
-		_perror(command, 2, data);
-		return;
-	}
-	cp_pwd = _strdup(pwd);
-	set_env("OLDPWD", cp_pwd, data);
-
-	data->_return = 0;
-	chdir(hold);
-	getcwd(pwd, sizeof(pwd));
-	cp_dir = _strdup(pwd);
-	set_env("PWD", cp_dir, data);
-
-	free(cp_pwd);
-	free(cp_dir);
-}
-/**
- * cd_previous - changes to previous directory
- * @data: struct data
- */
-void cd_previous(store *data)
-{
-	char pwd[PATH_MAX];
-	char *p_pwd, *p_oldpwd, *cp_pwd, *cp_oldpwd;
-
-	getcwd(pwd, sizeof(pwd));
-	cp_pwd = _strdup(pwd);
-
-	p_oldpwd = _getenv("OLDPWD", data->_environ);
-
-	if (p_oldpwd == NULL)
-		cp_oldpwd = cp_pwd;
-	else
-		cp_oldpwd = _strdup(p_oldpwd);
-	set_env("OLDPWD", cp_pwd, data);
-	if (chdir(cp_oldpwd) == -1)
-		set_env("PWD", cp_pwd, data);
-	else
-		set_env("PWD", cp_oldpwd, data);
-	p_pwd = _getenv("PWD", data->_environ);
-
-	write(STDOUT_FILENO, p_pwd, _strlen(p_pwd));
-	write(STDOUT_FILENO, "\n", 1);
-
-	free(cp_pwd);
-	if (p_oldpwd)
-		free(cp_oldpwd);
-
-	data->_return = 0;
-	chdir(p_pwd);
-}
-/**
- * cd_to_home - changes to home directory
- * @command: command passed
- * @data: struct store
- */
-void cd_to_home(char **command, store *data)
-{
-	char *p_pwd, *home;
-	char pwd[PATH_MAX];
-
-	getcwd(pwd, sizeof(pwd));
-	p_pwd = _strdup(pwd);
-
-	home = _getenv("HOME", data->_environ);
-	if (home == NULL)
-	{
-		set_env("OLDPWD", p_pwd, data);
-		free(p_pwd);
-		return;
-	}
-	if (chdir(home) == -1)
-	{
-		_perror(command, 2, data);
-		free(p_pwd);
-		return;
-	}
-	set_env("OLDPWD", p_pwd, data);
-	set_env("PWD", home, data);
-	free(p_pwd);
-	data->_return = 0;
+		deleteNodeAtIndex(&build->env, index);
+		addNodeAtIndex(&build->env, index, cwd);
+	} else
+		addNodeAtIndex(&build->env, 0, cwd);
+	insertNullByte(tmp, 0);
+	insertNullByte(cwd, 0);
+	return (true);
 }
