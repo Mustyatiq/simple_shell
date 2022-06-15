67fa1d5e04005cdb1deb54d8b1ddee3b7bdf8753
victornnamdii <101352050+victornnamdii@users.noreply.github.com>
2022-06-15 22:15:08 +0100

    Update strings2.c

--- strings2.c
+++ strings2.c
@@ -1,104 +1,66 @@
 #include "main.h"
+
 /**
- * _strdup - returns a pointer to a newly allocated space in memory,
- * which contains a copy of the string given as a parameter.
- * @str: char type pointer to string
- * Return: NULL if str is NULL, or insufficient memory available
- * On success, returns a pointer to the duplicated string.
+ * _strtok - tokenizes strings at delimiter
+ * @str: input string
+ * @delim: delimiter
+ * Return: pointer to start of string
  */
-char *_strdup(char *str)
+
+char *_strtok(char *str, char *delim)
 {
-	char *dstr;
-	unsigned int size, i;
+	static char *lastptr;
+	char ch;
 
 	if (str == NULL)
-		return  (NULL);
-	for (size = 0; str[size] != '\0'; size++)
-		;
-	dstr  = malloc((size + 1) * sizeof(char));
-
-	if (dstr == NULL)
-		return (NULL);
-	for  (i = 0; i <  size; i++)
-	{
-		dstr[i] = str[i];
-	}
-	dstr[size] = '\0';
-	return (dstr);
+		str = lastptr;
+	do {
+		ch = *str++;
+		if (!ch)
+			return (NULL);
+	} while (_strchr(delim, ch));
+	str--;
+	lastptr = str + _strcspn(str, delim);
+	if (*lastptr)
+		*lastptr++ = 0;
+	return (str);
 }
-/**
- * _strlen - returns the length of a string
- * @s: char type string
- * Return: string length
- */
-int _strlen(char *s)
-{
-	int i = 0;
 
-	while (s[i] != '\0')
-	{
-		i++;
-	}
-	return (i);
+/**
+ * _strcspn - returns first occurence of any char in
+ * second string in first string
+ * @string: input string to search
+ * @chars: input chars to check
+ * Return: pointer to first match
+ */
+int _strcspn(char *string, char *chars)
+{
+	char c;
+	char *p, *s;
+
+	for (s = string, c = *s; c; s++, c = *s)
+		for (p = chars; *p; p++)
+			if (c == *p)
+				return (s - string);
+	return (s - string);
 }
-/**
- * rev_string - reverses a string
- * @s: char type pointer
- */
-void rev_string(char *s)
-{
-	int i = 0, j, l = 0;
-	char k;
 
-	while (s[i] != '\0')
-	{
-		i++;
-	}
-	i--;
-	j = i;
-	while (l <= j / 2)
-	{
-		k = s[l];
-		s[l] = s[i];
-		s[i] = k;
-		i--;
-		l++;
-	}
-}
 /**
- * _isdigit - checks if character is a digit
- * @str: string to be checked
- * Return: 1 if it is a digit, 0 otherwise
+ * _strchr - locates a character in a string
+ * @s: string to be searched
+ * @c: target char
+ * Return: pointer to first occurrence of c or NULL if char not found
  */
-int _isdigit(const char *str)
+char *_strchr(char *s, char c)
 {
-	unsigned int i = 0;
+	char x;
 
-	for (i = 0; str[i]; i++)
+	while (true)
 	{
-		if (str[i] < 48 || str[i] > 57)
-			return (0);
+		x = *s++;
+		if (x == c)
+			return (s - 1);
+		if (!x)
+			return (NULL);
 	}
-		return (1);
-}
-/**
- * rpath - copies src to dest
- * @src: char type pointer
- * Return: src
- */
-char *rpath(char *src)
-{
-	int i, j;
-	char *hold;
-
-	hold = malloc((sizeof(char) * (_strlen(src) - 9)) + 1);
-	for (i = 9, j = 0; src[i] != '\0'; i++)
-	{
-		hold[j] = src[i];
-		j++;
-	}
-	hold[j] = '\0';
-	src = _strdup(hold);
-	free(hold);
-	return (src);
 }
