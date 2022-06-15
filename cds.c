040817c885113c8249f4e9d28937af599bee8cd0
victornnamdii <101352050+victornnamdii@users.noreply.github.com>
2022-06-15 23:08:04 +0100

    Update cds.c

--- cds.c
+++ cds.c
@@ -1,59 +1,165 @@
 #include "main.h"
 
 /**
- * updateOld - updates OLDPWD to current PWD
- * @build: input build
- * Return: index in linked list of PWD on success -
- * If PWD or OLDPWD does not exist in environ vars,
- * return -1
+ * cd_dot - changes to the parent directory
+ *
+ * @datash: data relevant (environ)
+ *
+ * Return: no return
  */
-int updateOld(config *build)
+void cd_dot(data_shell *datash)
 {
-	register int pwdIndex = 0, index = 0;
-	static char old[BUFSIZE];
-	char *current = NULL;
+	char pwd[PATH_MAX];
+	char *dir, *cp_pwd, *cp_strtok_pwd;
 
-	_strcat(old, "OLD");
-	pwdIndex = searchNode(build->env, "PWD");
-	if (pwdIndex == -1)
+	getcwd(pwd, sizeof(pwd));
+	cp_pwd = _strdup(pwd);
+	set_env("OLDPWD", cp_pwd, datash);
+	dir = datash->args[1];
+	if (_strcmp(".", dir) == 0)
 	{
-		return (-1);
+		set_env("PWD", cp_pwd, datash);
+		free(cp_pwd);
+		return;
 	}
-	current = getNodeAtIndex(build->env, pwdIndex);
-	_strcat(old, current);
-	insertNullByte(old, _strlen(current) + 4);
-	free(current);
-	index = searchNode(build->env, "OLDPWD");
-	if (index == -1)
+	if (_strcmp("/", cp_pwd) == 0)
 	{
-		return (-1);
+		free(cp_pwd);
+		return;
 	}
-	deleteNodeAtIndex(&build->env, index);
-	addNodeAtIndex(&build->env, index, old);
-	insertNullByte(old, 0);
-	return (pwdIndex);
+	cp_strtok_pwd = cp_pwd;
+	rev_string(cp_strtok_pwd);
+	cp_strtok_pwd = _strtok(cp_strtok_pwd, "/");
+	if (cp_strtok_pwd != NULL)
+	{
+		cp_strtok_pwd = _strtok(NULL, "\0");
+
+		if (cp_strtok_pwd != NULL)
+			rev_string(cp_strtok_pwd);
+	}
+	if (cp_strtok_pwd != NULL)
+	{
+		chdir(cp_strtok_pwd);
+		set_env("PWD", cp_strtok_pwd, datash);
+	}
+	else
+	{
+		chdir("/");
+		set_env("PWD", "/", datash);
+	}
+	datash->status = 0;
+	free(cp_pwd);
 }
 
 /**
- * updateCur - updates PWD to accurately reflect current directory
- * @build: input build
- * @index: index in linked list of where to insert PWD env var
- * Return: true on success, false on failure
+ * cd_to - changes to a directory given
+ * by the user
+ *
+ * @datash: data relevant (directories)
+ * Return: no return
  */
-_Bool updateCur(config *build, int index)
+void cd_to(data_shell *datash)
 {
-	static char tmp[BUFSIZE], cwd[BUFSIZE];
+	char pwd[PATH_MAX];
+	char *dir, *cp_pwd, *cp_dir;
 
-	getcwd(tmp, BUFSIZE);
-	_strcat(cwd, "PWD=");
-	_strcat(cwd, tmp);
-	if (index > -1)
+	getcwd(pwd, sizeof(pwd));
+
+	dir = datash->args[1];
+	if (chdir(dir) == -1)
 	{
-		deleteNodeAtIndex(&build->env, index);
-		addNodeAtIndex(&build->env, index, cwd);
-	} else
-		addNodeAtIndex(&build->env, 0, cwd);
-	insertNullByte(tmp, 0);
-	insertNullByte(cwd, 0);
-	return (true);
+		get_error(datash, 2);
+		return;
+	}
+
+	cp_pwd = _strdup(pwd);
+	set_env("OLDPWD", cp_pwd, datash);
+
+	cp_dir = _strdup(dir);
+	set_env("PWD", cp_dir, datash);
+
+	free(cp_pwd);
+	free(cp_dir);
+
+	datash->status = 0;
+
+	chdir(dir);
+}
+
+/**
+ * cd_previous - changes to the previous directory
+ *
+ * @datash: data relevant (environ)
+ * Return: no return
+ */
+void cd_previous(data_shell *datash)
+{
+	char pwd[PATH_MAX];
+	char *p_pwd, *p_oldpwd, *cp_pwd, *cp_oldpwd;
+
+	getcwd(pwd, sizeof(pwd));
+	cp_pwd = _strdup(pwd);
+
+	p_oldpwd = _getenv("OLDPWD", datash->_environ);
+
+	if (p_oldpwd == NULL)
+		cp_oldpwd = cp_pwd;
+	else
+		cp_oldpwd = _strdup(p_oldpwd);
+
+	set_env("OLDPWD", cp_pwd, datash);
+
+	if (chdir(cp_oldpwd) == -1)
+		set_env("PWD", cp_pwd, datash);
+	else
+		set_env("PWD", cp_oldpwd, datash);
+
+	p_pwd = _getenv("PWD", datash->_environ);
+
+	write(STDOUT_FILENO, p_pwd, _strlen(p_pwd));
+	write(STDOUT_FILENO, "\n", 1);
+
+	free(cp_pwd);
+	if (p_oldpwd)
+		free(cp_oldpwd);
+
+	datash->status = 0;
+
+	chdir(p_pwd);
+}
+
+/**
+ * cd_to_home - changes to home directory
+ *
+ * @datash: data relevant (environ)
+ * Return: no return
+ */
+void cd_to_home(data_shell *datash)
+{
+	char *p_pwd, *home;
+	char pwd[PATH_MAX];
+
+	getcwd(pwd, sizeof(pwd));
+	p_pwd = _strdup(pwd);
+
+	home = _getenv("HOME", datash->_environ);
+
+	if (home == NULL)
+	{
+		set_env("OLDPWD", p_pwd, datash);
+		free(p_pwd);
+		return;
+	}
+
+	if (chdir(home) == -1)
+	{
+		get_error(datash, 2);
+		free(p_pwd);
+		return;
+	}
+
+	set_env("OLDPWD", p_pwd, datash);
+	set_env("PWD", home, datash);
+	free(p_pwd);
+	datash->status = 0;
 }
